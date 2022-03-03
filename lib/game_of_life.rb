9df21c3029be912a78eb0ed45378f00937ea89c2
Joe Fong <joe.fong@madetech.com>
2022-03-03 11:01:37 +0000

    pass test for #count_living_neighbours in case of 1x3 and 3x1 grid. save progress before tackling 3x3 grid

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -10,7 +10,28 @@ class GameOfLife
   end
 
   def count_living_neighbours(current_grid, row_index, column_index)
-    0
+    # return 0 if current_grid.length == 0 && current_grid[row_index].length == 0
+
+    count = 0
+
+    coordinates_of_neighbour_cells = []
+    coordinates_of_neighbour_cells << [column_index - 1, row_index]
+    coordinates_of_neighbour_cells << [column_index + 1, row_index]
+    coordinates_of_neighbour_cells << [column_index, row_index - 1]
+    coordinates_of_neighbour_cells << [column_index, row_index + 1]
+
+    max_row_index = current_grid.length - 1
+    max_column_index = current_grid[row_index].length - 1
+
+    coordinates_of_neighbour_cells.filter! do |x, y|
+      x >= 0 && x <= max_column_index && y >= 0 && y <= max_row_index
+    end
+
+    coordinates_of_neighbour_cells.each do |x, y|
+      count += 1 if current_grid[y][x] == :live
+    end
+
+    count
   end
 
   def new_state_of_cell(current_state, number_of_living_neighbours)
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -64,7 +64,6 @@ describe GameOfLife do
             expect(actual_output).to eql expected_output
           end
         end
-
       end
 
       context 'die of over-population when number of living neighbour is 4 ~ 8' do
@@ -79,10 +78,160 @@ describe GameOfLife do
           end
         end
       end
-
     end
   end
 
   describe '#count_living_neighbours' do
+    current_cell_possible_states = [:live, :dead]
+    current_cell_possible_states.each do |current_cell|
+      context "when current cell is #{current_cell.inspect} and" do
+        it 'return 0 when being the only cell in a grid' do
+          grid = [[current_cell]]
+          row_index = 0
+          column_index = 0
+          expected_output = 0
+
+          actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+          expect(actual_output).to eql expected_output
+        end
+
+        context 'in a 1 x 3 grid, being the cell in the middle' do
+          it 'return 0 when both left and right are dead cell' do
+            grid = [[:dead, current_cell, :dead]]
+            row_index = 0
+            column_index = 1
+            expected_output = 0
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the left cell is alive' do
+            grid = [[:live, current_cell, :dead]]
+            row_index = 0
+            column_index = 1
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the right cell is alive' do
+            grid = [[:dead, current_cell, :live]]
+            row_index = 0
+            column_index = 1
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 2 when both the left and right cells are alive' do
+            grid = [[:live, current_cell, :live]]
+            row_index = 0
+            column_index = 1
+            expected_output = 2
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+        end
+
+        context 'in a 1x2 grid, being the cell at the left' do
+          it 'return 0 when the cell at the right is dead' do
+            grid = [[current_cell, :dead]]
+            row_index = 0
+            column_index = 0
+            expected_output = 0
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the cell at the right is alive' do
+            grid = [[current_cell, :live]]
+            row_index = 0
+            column_index = 0
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+        end
+
+        context 'in a 1x2 grid, being the cell at the right' do
+          it 'return 0 when the cell at the left is dead' do
+            grid = [[:dead, current_cell]]
+            row_index = 0
+            column_index = 1
+            expected_output = 0
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the cell at the left is alive' do
+            grid = [[:live, current_cell]]
+            row_index = 0
+            column_index = 1
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+        end
+
+
+        context 'in a 3 x 1 grid, being the cell in the middle' do
+          it 'return 0 when both upper and lower are dead cell' do
+            grid = [[:dead],
+                    [current_cell],
+                    [:dead]]
+            row_index = 1
+            column_index = 0
+            expected_output = 0
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the upper cell is alive' do
+            grid = [[:live],
+                    [current_cell],
+                    [:dead]]
+            row_index = 1
+            column_index = 0
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 1 when the lower cell is alive' do
+            grid = [[:dead],
+                    [current_cell],
+                    [:live]]
+            row_index = 1
+            column_index = 0
+            expected_output = 1
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+
+          it 'return 2 when both upper and lower cells are alive' do
+            grid = [[:live],
+                    [current_cell],
+                    [:live]]
+            row_index = 1
+            column_index = 0
+            expected_output = 2
+
+            actual_output = subject.count_living_neighbours(grid, row_index, column_index)
+            expect(actual_output).to eql expected_output
+          end
+        end
+      end
+    end
   end
 end
